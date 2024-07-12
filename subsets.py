cbff245df55c9df75908132b740ae7dfd5a74898
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2024-07-12 17:30:19 +0100

    solved leet code problems

---
+++ subsets.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        
+        result = []
+        subset = []
+        
+        def dfs(i):
+            if i>=len(nums):
+                result.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+            
+        dfs(0)
+        
+        return result
+
+res = Solution().subsets([1,2,3])
+
+print(res)
