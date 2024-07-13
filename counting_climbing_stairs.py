76def4f8d1dc0c0a97e5416e5721308b40579218
Vikrant2691 <37099198+Vikrant2691@users.noreply.github.com>
2024-07-13 20:41:42 +0100

    solved leet code problems

--- counting_climbing_stairs.py
+++ counting_climbing_stairs.py
@@ -12,14 +12,12 @@ class Solution:
         end = len(cost)-2
         
         while end>=0:
-            temp = cost[end] + min(n0,n1)k
+            temp = cost[end] + min(n0,n1)
             n1 = n0
             n0 = temp
             end-=1
             
         return min(n0,n1)
-    
-    
     
 res = Solution().minCostClimbingStairs([1,2,3])
 print(res)
